02a423a4d45fc752d2a288fe1bb7f8c284c1f88a
IvanPribytkov <pribytkov.ivan@gmail.com>
2023-09-27 10:15:16 +0500

    Работа теста 93%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -7,9 +7,7 @@ def test_get():
     assert arrs.get([10, 20, 30], 0, "test") == 10
     assert arrs.get([1, 2, 3, 4], 3, "test") == 4
     assert arrs.get([1, 2, 3], -1, "test") == "test"
-    assert arrs.get([1, 2, 3], 0, "test") == 1
-    assert arrs.my_slice([1, 2, 3, 4, 5], 2) == [3, 4, 5]
-    assert arrs.get([1, "two", 3.0], 2) == 3.0
+    assert arrs.get([1, 2, 3], 10, "test") == "test"
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
@@ -18,5 +16,3 @@ def test_slice():
     assert arrs.my_slice([1, 2, 3, 4, 5], -3) == [3, 4, 5]
     assert arrs.my_slice([], 0, 2) == []
     assert arrs.my_slice([], None, None) == []
-    assert arrs.get([1, "two", 3.0], 2) == 3.0
-    assert arrs.my_slice([1, 2, 3, 4, 5], 1, 4) == [2, 3, 4]
