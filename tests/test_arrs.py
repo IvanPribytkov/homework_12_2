f4465b197d61b5a358c465a764b5ec31489652de
IvanPribytkov <pribytkov.ivan@gmail.com>
2023-10-02 16:49:07 +0500

    Работа теста 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -16,3 +16,5 @@ def test_slice():
     assert arrs.my_slice([1, 2, 3, 4, 5], -3) == [3, 4, 5]
     assert arrs.my_slice([], 0, 2) == []
     assert arrs.my_slice([], None, None) == []
+    assert arrs.my_slice([None], None, None)
+
